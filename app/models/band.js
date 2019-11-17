95598e9db6ecf151a27e6ddab4eae7ff4a92ec91
Russell Snyder <russellsnyder@Russells-MBP-2.fritz.box>
2019-11-17 13:59:31 +0100

    feature: chapter 6 complete

---
+++ app/models/band.js
@@ -0,0 +1,14 @@
+import EmberObject from '@ember/object';
+
+export default EmberObject.extend({
+  name: '',
+
+  init() {
+
+    this._super(...arguments);
+
+    if (!this.songs) {
+      this.set('songs', []);
+    }
+  },
+});
