252e7f90341b8d1d52156a4b32d1ac1b91d3eb70
Russell Snyder <russellsnyder@Russells-MBP-2.fritz.box>
2019-11-24 18:37:37 +0100

    Begin chapter 12 and commit where suggested

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return this.modelFor('bands.band');
+  },
+
   resetController(controller) {
     controller.setProperties({
       isAddingSong: false,
